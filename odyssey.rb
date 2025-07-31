790d6f5b036c2aceee422ef06deee1c080b6f715
chinmay1088 <ghulechinmay68@gmail.com>
2025-07-31 20:02:18 +0530

    sha256 edit

--- odyssey.rb
+++ odyssey.rb
@@ -2,7 +2,7 @@ class Odyssey < Formula
   desc "Secure command-line cryptocurrency wallet supporting Ethereum, Bitcoin, and Solana"
   homepage "https://github.com/chinmay1088/odyssey"
   url "https://github.com/chinmay1088/odyssey/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "297fc8fbb5a6d6eb0ceeecd131cac1421b3bb0f30ce831ef2a159d5a1a2ff558" 
+  sha256 "3bd4bf130adcaa255d65fa9dc06ce459fbbe6dd11c92ea727dd7544870a739ed"
   license "MIT"
   head "https://github.com/chinmay1088/odyssey.git", branch: "main"
 
